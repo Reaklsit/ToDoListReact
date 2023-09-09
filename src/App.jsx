c9a83cbb5942100d1f16c77b81c8ff040199eec1
Tymofii Popov <halatik2019@gmail.com>
2023-09-09 20:16:59 +0200

    fix bag

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import Counter from "./components/Counter/counter"
-import Form from "./components/Form/Form"
-import MyTodoList from "./components/MyTodoList/MyTodoList"
-import PropsData from "./components/PropsData/PropsData"
-import TodoList from "./components/TodoList/TodoList"
-import UserList from "./components/UserList/UserList"
+// import Counter from "./components/Counter/counter"
+// import Form from "./components/Form/Form"
+import MyTodoList from "./components/MyTodoList/MyTodoList.jsx"
+// import PropsData from "./components/PropsData/PropsData"
+// import TodoList from "./components/TodoList/TodoList"
+// import UserList from "./components/UserList/UserList"
 
 
 function App() {
